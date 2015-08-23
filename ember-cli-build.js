b541cafef2997a0c421a1cae5569b6384de6d033
Rotem Yaari <vmalloc@gmail.com>
2015-08-23 22:39:50 +0300

    Font Awesome

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+      emberCliFontAwesome: {
+          useScss: true
+      }
     // Add options here
   });
 
